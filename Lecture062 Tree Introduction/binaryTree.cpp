b9aed65b3602ee7abfde5070fa64deb6e61a3553
tejaswa-jaiswal <159774410+tejaswa-jaiswal@users.noreply.github.com>
2024-11-27 00:01:40 +0530

    Update binaryTree.cpp
    line 50 ndoes to nodes

--- Lecture062 Tree Introduction/binaryTree.cpp
+++ Lecture062 Tree Introduction/binaryTree.cpp
@@ -47,7 +47,7 @@ void levelOrderTraversal(node* root) {
             //purana level complete traverse ho chuka hai
             cout << endl;
             if(!q.empty()) { 
-                //queue still has some child ndoes
+                //queue still has some child nodes
                 q.push(NULL);
             }  
         }
